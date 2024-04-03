15e9c754a553ad70570ef02e8a4d89759837f82c
Edmund Miller <git@edmundmiller.dev>
2024-04-02 22:19:09 -0500

    build: devtools => remotes

--- rDeps.R
+++ rDeps.R
@@ -6,10 +6,9 @@ install.packages("randomForest", repos = "http://cran.us.r-project.org")
 install.packages("rmutil", repos = "http://cran.us.r-project.org")
 install.packages("rphast", repos = "http://cran.us.r-project.org")
 install.packages("snowfall", repos = "http://cran.us.r-project.org")
-install.packages("devtools", repos = "http://cran.us.r-project.org")
-library(devtools)
+install.packages("remotes")
 # Here a bug in current devtools 1.12.0 if the installation starts from a sub-folder.
-r <- try( install_github("andrelmartins/bigWig", subdir="bigWig", force=TRUE) )
+r <- try(remotes::install_github("andrelmartins/bigWig", subdir="bigWig", force=TRUE) )
 if ( class( r )== "try-error" )
 	stop("!!!!!Failed to install bigWig from github using install_github method.!!!!\n 
 	You can install manually using the following commands:\n
